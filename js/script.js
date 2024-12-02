2d38bdb5d55f9123e1a4f9b9f11e25c261620168
German Lacruz <lacruz.german@gmail.com>
2024-12-01 22:05:09 -0300

    Se configura Email.js 01/12/2024

--- js/script.js
+++ js/script.js
@@ -1,33 +1,4 @@
-// const btn = document.getElementById("sendEmail");
 
-// document.getElementById("formContact").addEventListener("submit", function(event) {
-//   event.preventDefault();
-
-//   const serviceID = "service_0k4vjla"; //Remplazar 
-//   const templateID = "template_5sj22co"; //Remplazar 
-
-//   emailjs.sendForm(serviceID, templateID, this).then(
-//     () => {
-//       Swal.fire(
-//         'Message sent succesfully',
-//         'Our team will contact you shortly',
-//         'success'
-//       )
-//     },
-//     err => {
-//       Swal.fire(
-//         'Message not sent',
-//         'Try again later.',
-//         'error'
-//       )
-//     }
-//   );
-//   document.getElementById("name").value ="";
-//   document.getElementById("lastName").value ="";
-//   document.getElementById("email").value ="";
-//  // document.getElementById("asunto").value ="";
-//   document.getElementById("message").value ="";
-// });
 
 // Seleccionamos el navbar y la sección "ourMision"
 window.onload = function() {
@@ -45,75 +16,6 @@ window.onload = function() {
   }
   window.addEventListener('scroll', checkPosition);
 };
-
-/* 
-
-// Variables para manejar el carrusel
-const wrapper = document.querySelector('.carousel-wrapper');
-const cards = document.querySelectorAll('.card--services');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-
-let currentIndex = 0; // Índice inicial
-let autoSlideInterval; // Intervalo de autoavance
-
-// Función para calcular el ancho de una tarjeta
-function getCardWidth() {
-    const width = window.innerWidth;
-    if (width <= 414) return cards[0].offsetWidth + 15; // Margen pequeño para vistas móviles
-    return cards[0].offsetWidth + 60; // Margen estándar para pantallas más grandes
-}
-
-// Función para actualizar la posición del carrusel
-function updateCarousel() {
-    const cardWidth = getCardWidth();
-    wrapper.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-}
-
-// Función para avanzar el carrusel
-function nextCard() {
-    if (currentIndex < cards.length - 1) {
-        currentIndex++;
-    } else {
-        currentIndex = 0; // Volver al inicio
-    }
-    updateCarousel();
-}
-
-// Función para retroceder el carrusel
-function prevCard() {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = cards.length - 1; // Volver al final
-    }
-    updateCarousel();
-}
-
-// Función para autoavance
-function startAutoSlide() {
-    autoSlideInterval = setInterval(nextCard, 6000);
-}
-
-// Función para detener el autoavance
-function stopAutoSlide() {
-    clearInterval(autoSlideInterval);
-}
-
-// Eventos
-nextBtn.addEventListener('click', nextCard);
-prevBtn.addEventListener('click', prevCard);
-wrapper.addEventListener('mouseenter', stopAutoSlide);
-wrapper.addEventListener('mouseleave', startAutoSlide);
-window.addEventListener('resize', () => {
-    currentIndex = 0; // Reiniciar posición para evitar cortes
-    updateCarousel();
-});
-
-// Inicialización
-updateCarousel();
-startAutoSlide();
- */
 
 
   document.addEventListener('DOMContentLoaded', function () {
@@ -148,3 +50,34 @@ cards.forEach(card => {
   });
 });
 
+const btn = document.getElementById("sendEmail");
+
+document.getElementById("formContact").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const serviceID = "service_rh6fyh3"; //Remplazar 
+  const templateID = "template_jqpykq9"; //Remplazar 
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      Swal.fire(
+        'Message sent succesfully',
+        'Our team will contact you shortly',
+        'success'
+      )
+    },
+    err => {
+      Swal.fire(
+        'Message not sent',
+        'Try again later.',
+        'error'
+      )
+    }
+  );
+  document.getElementById("firstName").value ="";
+  document.getElementById("email").value ="";
+  document.getElementById("service").value ="";
+  document.getElementById("frequency").value ="";
+  document.getElementById("address").value ="";
+  document.getElementById("comments").value ="";
+});
